85adff501ee5163c1e44b6bf89908990d939c129
ryo.nagata <ryo.nagata@chillnn.com>
2021-11-27 14:17:24 +0900

    update

--- cleanSchema.sh
+++ cleanSchema.sh
@@ -3,5 +3,5 @@ yarn codegen &&\
 # userの無駄なコード削除
 sed -i -e '/scalar AWS/d' ./resources/appsync/user/schema.graphql &&\
 sed -i -e '/^$/d' ./resources/appsync/user/schema.graphql &&\
-rm ./resources/appsync/user/schema.graphq &&\
+rm ./resources/appsync/user/schema.graphql &&\
 sls deploy
